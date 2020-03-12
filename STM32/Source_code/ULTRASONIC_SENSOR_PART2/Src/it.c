c43ff1bbcd76da1820e2ee356bd89244b1592c3d
Anitha241991 <61533638+Anitha241991@users.noreply.github.com>
2020-03-12 17:27:01 +0530

    Update it.c

--- STM32/Source_code/ULTRASONIC_SENSOR_PART2/Src/it.c
+++ STM32/Source_code/ULTRASONIC_SENSOR_PART2/Src/it.c
@@ -1,14 +1,13 @@
-
 /*
  * it.c
  *
  *  Created on: 02-Jun-2018
- *      Author: kiran
+ *      Author: Dell
  */
 
 
 #include "main.h"
-
+extern TIM_HandleTypeDef htimer1;
 extern TIM_HandleTypeDef htimer3;
 
 void SysTick_Handler (void)
@@ -16,6 +15,12 @@ void SysTick_Handler (void)
 	HAL_IncTick();
 	HAL_SYSTICK_IRQHandler();
 }
+
+void TIM1_IRQHandler(void)
+	{
+
+		HAL_TIM_IRQHandler(&htimer1);
+	}
 
 
 void TIM3_IRQHandler  (void)
