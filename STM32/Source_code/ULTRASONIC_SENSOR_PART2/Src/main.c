58b89567935d65eb468e9c5e8b87839bac7f85ff
Anitha241991 <61533638+Anitha241991@users.noreply.github.com>
2020-03-20 14:52:26 +0530

    Update main.c

--- STM32/Source_code/ULTRASONIC_SENSOR_PART2/Src/main.c
+++ STM32/Source_code/ULTRASONIC_SENSOR_PART2/Src/main.c
@@ -210,16 +210,6 @@ void GPIO_Init(void)
 
 }
 
-//void usDelay(uint32_t uSec)
-//{
-//	if(uSec < 2) uSec = 2;
-//	usTIM->ARR = uSec - 1; 	/*sets the value in the auto-reload register*/
-//	usTIM->EGR = 1; 			/*Re-initialises the timer*/
-//	usTIM->SR &= ~1; 		//Resets the flag
-//	usTIM->CR1 |= 1; 		//Enables the counter
-//	while((usTIM->SR&0x0001) != 1);
-//	usTIM->SR &= ~(0x0001);
-//}
 
 void delay_us(uint32_t us)
 {
